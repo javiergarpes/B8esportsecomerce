317bcfeec71e28acd2adaf394eb2d31d45e51782
javiergarpes <javagagar29@gmail.com>
2023-09-14 22:38:58 -0400

    .

--- src/screens/Details/Details.jsx
+++ src/screens/Details/Details.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import styles from './Details.style'
 
 const Details = ({ product }) => {
-  
+  //
   return (
     <View style={styles.container}>
       <Header title={'Detalle'} />
