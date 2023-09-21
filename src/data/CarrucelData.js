9afb4fd5bcc056ea3433d99bc52924447f27245f
javiergarpes <javagagar29@gmail.com>
2023-09-21 00:43:50 -0400

    Actualizacion de las imágenes del carrusel

--- src/data/CarrucelData.js
+++ src/data/CarrucelData.js
@@ -1,39 +1,59 @@
 export default[
     
         {
-          "id": 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-          title: 'First Item',
-          images:"https://w7.pngwing.com/pngs/575/406/png-transparent-clash-royale-clash-of-clans-computer-icons-monster-legends-rpg-clash-of-clans-game-fictional-character-smile.png"
-        },
-        {
-          "id": '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          title: 'Second Item',
-          images:"https://cdna.artstation.com/p/assets/images/images/029/816/228/large/brice-laville-saint-martin-app-icon-cr-2020.jpg?1692911026"
-        },
-        {
-          id: '58694a0f-3da1-471f-bd96-145571e29d72',
-          title: 'Third Item',
-          images:"https://w7.pngwing.com/pngs/575/406/png-transparent-clash-royale-clash-of-clans-computer-icons-monster-legends-rpg-clash-of-clans-game-fictional-character-smile.png"
+          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+          title: 'Riot Games',
+          images:"https://w0.peakpx.com/wallpaper/615/630/HD-wallpaper-riot-fist-black-league-of-legends-logo-riot-games-riot-games-fist-riot-games-riot-logo-riot-thumbnail.jpg"
         },
         {
           id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          title: 'Second Item',
+          title: 'Clash Royale',
           images:"https://cdna.artstation.com/p/assets/images/images/029/816/228/large/brice-laville-saint-martin-app-icon-cr-2020.jpg?1692911026"
         },
         {
-          id: '58694a0f-3da1-471f-bd96-145571e29d72',
-          title: 'Third Item',
-          images:"https://w7.pngwing.com/pngs/575/406/png-transparent-clash-royale-clash-of-clans-computer-icons-monster-legends-rpg-clash-of-clans-game-fictional-character-smile.png"
+          id: '76561198021382987',
+          title: 'Counter-Strike: Global Offensive',
+          images: 
+            'https://w0.peakpx.com/wallpaper/727/192/HD-wallpaper-counter-strike-global-offensive-minimal-black-backgrounds-cs-go-creative-cs-go-logo-counter-strike.jpg',
+          
         },
         {
-          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          title: 'Second Item',
-          images:"https://cdna.artstation.com/p/assets/images/images/029/816/228/large/brice-laville-saint-martin-app-icon-cr-2020.jpg?1692911026"
+          id: '279530',
+          title: 'Dota 2',
+          images: 
+            'https://1000marcas.net/wp-content/uploads/2020/02/Dota-2-S%C3%ADmbolo.jpg',
+          
+        },
+
+        {
+          id: '292030',
+          title: 'Minecraft',
+          images: 
+            'https://i.pinimg.com/736x/3c/5c/27/3c5c2780a5164f40a5fa5b5128ada918.jpg',
+          
         },
         {
-          id: '58694a0f-3da1-471f-bd96-145571e29d72',
-          title: 'Third Item',
-          images:"https://w7.pngwing.com/pngs/575/406/png-transparent-clash-royale-clash-of-clans-computer-icons-monster-legends-rpg-clash-of-clans-game-fictional-character-smile.png"
+          id: '207740',
+          title: 'PUBG: Battlegrounds',
+          images: 
+            'https://e1.pxfuel.com/desktop-wallpaper/845/1018/desktop-wallpaper-pubg-png-pubg-character-pubg-games-logo-pubg-icon.jpg',
+          
         },
+        
+        {
+          id: '252950',
+          title: 'Fortnite',
+          images: 
+            'https://assets.stickpng.com/thumbs/5ef09433f3d0af0004ad1773.png',
+          
+        },
+        {
+          id: '109150',
+          title: 'Rocket League',
+          images: 
+            'https://i.pinimg.com/originals/69/01/d8/6901d8d4443067b68d2bd4a945659ca7.jpg',
+          
+        },
+
       
 ]
