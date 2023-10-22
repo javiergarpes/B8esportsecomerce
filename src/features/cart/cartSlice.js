16590613d04e740866c0b95c7e4560799b6e4035
javiergarpes <javagagar29@gmail.com>
2023-10-21 21:24:51 -0400

    .

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -12,35 +12,32 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addItem: (state, action) => {
-      const productRepeated = state.items.find(
+      const products = state.items
+      const productRepeated = products.find(
         item => item.id === action.payload.id
       )
-      if (productRepeated) {
-        const itemsUpdated = state.items.map(item => {
-          if (item.id === action.payload.id) {
-            item.quantity += action.payload.quantity
-            return item
-          }
-          return item
-        })
-        const newtotal = itemsUpdated.reduce(
-          (acc, current) => (acc += current.price * current.quantity),
-          0
-        )
-        console.log('este es el total', newtotal)
-        return  {
+
+      if (!productRepeated)
+        return {
           ...state,
-          items: itemsUpdated,
-          total:0,
+          items: [...state.items, action.payload],
+          total: state.total + action.payload.price,
           updatedAt: new Date().toLocaleString(),
         }
-      } else {
-        state.items.push(action.payload)
-        const total = state.items.reduce(
-          (acc, current) => (acc += current.price * current.quantity),
-          0
-        )
-        
+
+      const itemsUpdated = products.map(item => {
+        if (item.id === action.payload.id) {
+          return Object.assign({}, item, {
+            quantity: item.quantity + action.payload.quantity,
+          })
+        }
+        return item
+      })
+      return {
+        ...state,
+        items: itemsUpdated,
+        total: state.total + action.payload.price,
+        updatedAt: new Date().toLocaleString(),
       }
     },
     removeItem: (state, action) => {},
